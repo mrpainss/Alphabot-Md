20db3a5e7c957b643e69deaf50bbf19a11fa41df
mrpainss <105528316+mrpainss@users.noreply.github.com>
2022-05-28 17:58:12 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,7 +8,7 @@ global.APIs = {
 
 // Free apikey
 global.APIKeys = {
-	'https://api.zeeoneofc.xyz': 'your apikey',  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
+	'https://api.zeeoneofc.xyz': 'GQuuwYNF',  // login https://api.zeeoneofc.xyz to get apikey || https://api-alphabot.herokuapp.com
 }
 
 // setting 
